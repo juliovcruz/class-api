1fdd166f0f316d58b2fe0e1fba692198fcec9fec
Julio Cruz <julio.cruz@mercadolivre.com>
2022-09-11 03:48:33 -0300

    fix: allow headers cors

--- cmd/api/handlers/routes.go
+++ cmd/api/handlers/routes.go
@@ -37,8 +37,8 @@ func CORS() gin.HandlerFunc {
 			ctx.Header("Access-Control-Max-Age", "1728000")
 			ctx.Header("Access-Control-Allow-Credentials", "true")
 			ctx.Header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS")
-			ctx.Header("Access-Control-Allow-Origin", ctx.Request.Header.Get("Access-Control-Allow-Origin"))
-			ctx.Header("Access-Control-Allow-Headers", "Content-Type,Cookie,Authorization,Access-Control-Request-Headers,Access-Control-Request-Method,Origin,Referer,Sec-Fetch-Dest,Accept-Language,Accept-Encoding,Sec-Fetch-Mode,Sec-Fetch-Site,User-Agent,Pragma,Host,Connection,Cache-Control,Accept-Language,Accept-Encoding,X-Requested-With,X-Forwarded-For,X-Forwarded-Host,X-Forwarded-Proto,X-Forwarded-Port,X-Forwarded-Prefix,X-Real-IP,Accept")
+			ctx.Header("Access-Control-Allow-Origin", "*")
+			ctx.Header("Access-Control-Allow-Headers", "*")
 			ctx.AbortWithStatus(http.StatusNoContent)
 			return
 		}
