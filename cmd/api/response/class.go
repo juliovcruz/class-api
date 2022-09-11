4b54d0b0ae7e569eba5008008089969da474c3e4
Julio Cruz <julio.cruz@mercadolivre.com>
2022-09-11 18:58:51 -0300

    feat: add description to response struct

--- cmd/api/response/class.go
+++ cmd/api/response/class.go
@@ -7,24 +7,26 @@ import (
 )
 
 type ClassResponse struct {
-	ID         uuid.UUID `json:"id"`
-	Name       string    `json:"name"`
-	TotalHours int       `json:"total_hours"`
-	Schedules  string    `json:"schedules"` // 2N2345
-	TeacherID  uuid.UUID `json:"teacher_id"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	TotalHours  int       `json:"total_hours"`
+	Schedules   string    `json:"schedules"` // 2N2345
+	Description string    `json:"description"`
+	TeacherID   uuid.UUID `json:"teacher_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 func NewClassResponse(class entity.Class) ClassResponse {
 	return ClassResponse{
-		ID:         class.ID,
-		Name:       class.Name,
-		TotalHours: class.TotalHours,
-		Schedules:  class.Schedules,
-		TeacherID:  class.TeacherID,
-		CreatedAt:  class.CreatedAt,
-		UpdatedAt:  class.UpdatedAt,
+		ID:          class.ID,
+		Name:        class.Name,
+		TotalHours:  class.TotalHours,
+		Description: class.Description,
+		Schedules:   class.Schedules,
+		TeacherID:   class.TeacherID,
+		CreatedAt:   class.CreatedAt,
+		UpdatedAt:   class.UpdatedAt,
 	}
 }
 
@@ -33,13 +35,14 @@ func NewClassesResponse(classes []entity.Class) []ClassResponse {
 
 	for _, class := range classes {
 		res = append(res, ClassResponse{
-			ID:         class.ID,
-			Name:       class.Name,
-			TotalHours: class.TotalHours,
-			Schedules:  class.Schedules,
-			TeacherID:  class.TeacherID,
-			CreatedAt:  class.CreatedAt,
-			UpdatedAt:  class.UpdatedAt,
+			ID:          class.ID,
+			Name:        class.Name,
+			TotalHours:  class.TotalHours,
+			Description: class.Description,
+			Schedules:   class.Schedules,
+			TeacherID:   class.TeacherID,
+			CreatedAt:   class.CreatedAt,
+			UpdatedAt:   class.UpdatedAt,
 		})
 	}
 
